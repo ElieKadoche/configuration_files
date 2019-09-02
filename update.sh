cb2f767eed35272f02fc5875c09c1967c2461f3b
Elie KADOCHE <eliekadoche78@gmail.com>
2019-09-02 11:05:06 +0200

    Add update script

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+brew update
+
+brew upgrade git
+brew upgrade python3
+brew upgrade coreutils
+brew upgrade emojify
+brew upgrade macvim
+brew upgrade mpv
+brew upgrade unrar
+brew upgrade pandoc
+brew upgrade osx-cpu-temp
+brew upgrade cmake
+brew upgrade htop
+brew upgrade watch
+brew upgrade tldr
+
+brew cask upgrade firefox
+brew cask upgrade font-source-sans-pro
+brew cask upgrade mpv
+brew cask upgrade adobe-acrobat-reader
+brew cask upgrade texstudio
+brew cask upgrade mactex
+brew cask upgrade spectacle
+brew cask upgrade discord
+brew cask upgrade teamviewer
+brew cask upgrade lulu
+brew cask upgrade cgoban
+brew cask upgrade gopanda
+brew cask upgrade libreoffice
