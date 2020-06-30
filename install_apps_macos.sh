7c991f9096e4945cf69c7200da4205b4549a99e9
Elie KADOCHE <eliekadoche78@gmail.com>
2020-06-30 09:26:51 +0200

    Google Earth

--- install_apps_macos.sh
+++ install_apps_macos.sh
@@ -107,6 +107,7 @@ brew cask install firefox
 brew cask install flash-player
 brew cask install font-source-sans-pro
 brew cask install gimp
+brew cask install google-earth-pro
 brew cask install gopanda
 brew cask install intellij-idea-ce
 brew cask install libreoffice
