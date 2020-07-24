b402aceb424a758c969d8fea9900a3c9cd6d4054
Elie KADOCHE <eliekadoche78@gmail.com>
2020-07-24 10:13:37 +0200

    Added alacritty

--- install_apps_macos.sh
+++ install_apps_macos.sh
@@ -101,6 +101,7 @@ brew install wget
 echo "Running brew cask..."
 
 brew cask install android-file-transfer
+brew cask install alacritty
 brew cask install balenaetcher
 brew cask install blender
 brew cask install cloudcompare
--- install_apps_ubuntu.sh
+++ install_apps_ubuntu.sh
@@ -53,6 +53,7 @@ sudo apt install wget
 
 # From universe
 sudo apt install audacity
+sudo apt install alacritty
 sudo apt install bibclean
 sudo apt install blender
 sudo apt install clang-format
