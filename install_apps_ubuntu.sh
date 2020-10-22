8ea8bad4133400a304ccb5a0aba71f135a57b2b0
Elie KADOCHE <eliekadoche78@gmail.com>
2020-10-22 21:56:56 +0200

    Vim plugins installation

--- install_apps_ubuntu.sh
+++ install_apps_ubuntu.sh
@@ -233,6 +233,9 @@ echo "Installing Vim Plug..."
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+nvim +"PlugInstall" +qa;
+nvim +"CocInstall coc-texlab" +qa;
+
 # Python packages
 # ------------------------------------------
 
