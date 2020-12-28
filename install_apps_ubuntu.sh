935a22604ede7b761cf9941a17dd69ce284bac6b
Elie KADOCHE <eliekadoche78@gmail.com>
2020-12-28 10:02:50 +0100

    Remove plymouth

--- install_apps_ubuntu.sh
+++ install_apps_ubuntu.sh
@@ -292,6 +292,7 @@ cp others/alacritty.yml ~/.config/alacritty/alacritty.yml
 # Others: CGoban, Source-Sans-Pro, GoPanda, TeamViewer, Google Earth, Google Chrome
 # Install https://github.com/libjpeg-turbo/libjpeg-turbo/releases/
 # Install fzf from the source directory
+# Remove plymouth: [sudo vim /etc/default/grub] and remove splash
 
 # Extensions
 # ------------------------------------------
