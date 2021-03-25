50f47d752d9a81990fa2d8b03141f70ce1fda7cf
Elie KADOCHE <eliekadoche78@gmail.com>
2021-03-25 23:06:17 +0100

    Added tkinter

--- install_apps_ubuntu.sh
+++ install_apps_ubuntu.sh
@@ -107,6 +107,7 @@ sudo apt -y install python3-cartopy
 sudo apt -y install python3-distutils-extra
 sudo apt -y install python3-pip
 sudo apt -y install python3-pygame
+sudo apt -y install python3-tk
 sudo apt -y install python3-venv
 sudo apt -y install rawtherapee
 sudo apt -y install sassc
