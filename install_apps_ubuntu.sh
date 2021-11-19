91347ce89527e3376b12bd06ac21a28a2d608366
Elie KADOCHE <eliekadoche78@gmail.com>
2021-11-19 19:49:26 +0100

    Added dos2unix

--- install_apps_ubuntu.sh
+++ install_apps_ubuntu.sh
@@ -65,6 +65,7 @@ sudo apt -y install clang-format
 sudo apt -y install clinfo
 sudo apt -y install cloudcompare
 sudo apt -y install cmatrix
+sudo apt -y install dos2unix
 sudo apt -y install exfat-fuse
 sudo apt -y install ffmpeg
 sudo apt -y install ffmpegthumbnailer
