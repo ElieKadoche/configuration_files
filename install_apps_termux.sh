c7aa2a47937ebb6a5aa48b9bcd30ea5a09218b4d
Elie KADOCHE <eliekadoche78@gmail.com>
2023-04-05 00:49:42 +0200

    Added python-pip

--- install_apps_termux.sh
+++ install_apps_termux.sh
@@ -53,6 +53,7 @@ pkg install -y pastel
 pkg install -y pwgen
 pkg install -y python
 pkg install -y python-numpy
+pkg install -y python-pip
 pkg install -y rsync
 pkg install -y ruby
 pkg install -y screen
