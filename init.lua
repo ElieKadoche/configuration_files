30018a2e158cd04fef9f87a8366b1b51afc237dd
Elie KADOCHE <eliekadoche78@gmail.com>
2025-04-09 20:17:52 +0200

    Update colors

--- init.lua
+++ init.lua
@@ -725,17 +725,22 @@ plt.show()]],
 })
 
 -- Use vim colorscheme
--- Change colors (use :highlight)
+-- Change colors (use :highlight and :Inspect)
 vim.cmd("colorscheme vim")
 
 -- Main
 vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#3b3b3b" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#3b3b3b" })
 vim.api.nvim_set_hl(0, "LineNr", { bg = "#000000", fg = "#00ff00" })
-vim.api.nvim_set_hl(0, "Normal", { bg = "#000000", fg = "#ffffff" })
 vim.api.nvim_set_hl(0, "StatusLine", { bg = "#00ff00", fg = "#000000", bold = true })
 vim.api.nvim_set_hl(0, "TabLineFill", { bg = "#000000" })
-vim.api.nvim_set_hl(0, "Type", { bg = "#000000", fg = "#ffffff" })
+
+-- Syntax
+vim.api.nvim_set_hl(0, "@function.method.call.python", { bg = "#000000", fg = "#ff1493" })
+vim.api.nvim_set_hl(0, "@variable.member.python", { bg = "#000000", fg = "#ff00ff" })
+vim.api.nvim_set_hl(0, "@variable.python", { bg = "#000000", fg = "#00ffff" })
+vim.api.nvim_set_hl(0, "Constant", { bg = "#000000", fg = "#a9a9a9" })
+vim.api.nvim_set_hl(0, "Special", { bg = "#000000", fg = "#00ff00" })
 
 -- nvim-tree
 vim.api.nvim_set_hl(0, "NvimTreeExecFile", { fg = "#ffffff" })
@@ -743,6 +748,7 @@ vim.api.nvim_set_hl(0, "NvimTreeFileName", { fg = "#ffffff", bg = "#000000" })
 vim.api.nvim_set_hl(0, "NvimTreeFolderName", { fg = "#ff0000" })
 vim.api.nvim_set_hl(0, "NvimTreeModifiedFile", { fg = "#ffffff" })
 vim.api.nvim_set_hl(0, "NvimTreeOpenedFile", { fg = "#ffffff", bold = true })
+vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { fg = "#ff0000" })
 vim.api.nvim_set_hl(0, "NvimTreeSymlink", { fg = "#ffffff", italic = true })
 vim.api.nvim_set_hl(0, "NvimTreeWindowPicker", { bg = "#00ff00", fg = "#000000" })
 
